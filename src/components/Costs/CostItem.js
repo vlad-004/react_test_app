2706d1501735f92b56b78b82bdd05660e12d8a8f
vlad-004 <prostoy-vlad@rambler.ru>
2023-01-24 14:57:10 +0200

    добавил новое событие в компонент

--- src/components/Costs/CostItem.js
+++ src/components/Costs/CostItem.js
@@ -4,19 +4,25 @@ import Card from "../UI/Card";
 
 /**
  * Использования этого компонента - пример композиции приложения,
- * для общей композиции используем вместе компоненты + базовый html 
- * 
- * 
- * @param {*} props 
- * @returns 
+ * для общей композиции используем вместе компоненты + базовый html
+ *
+ *
+ * @param {*} props
+ * @returns
  */
 function CostItem(props) {
+    function changeDescriptionHandler() {
+        alert('click to change desription');
+    }
   return (
     <Card className="cost-item">
       <CostDate date={props.date} />
       <div className="cost-item__description">
         <h2>{props.description}</h2>
         <div className="cost-item__price">$ {props.amount}</div>
+          <button onClick={changeDescriptionHandler}>
+              CHange desription
+          </button>
       </div>
     </Card>
   );
