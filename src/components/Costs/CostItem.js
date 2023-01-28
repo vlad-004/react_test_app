1c2902f831daaddbef9de5e09a7df1f06ee70077
vlad-004 <prostoy-vlad@rambler.ru>
2023-01-28 02:04:44 +0200

    из компонента CostItem.js сделал глупый - компонент без внутреннего состояния. Используемый только для вывода данных.

--- src/components/Costs/CostItem.js
+++ src/components/Costs/CostItem.js
@@ -1,7 +1,6 @@
 import "./CostItem.css";
 import CostDate from "./CostDate";
 import Card from "../UI/Card";
-import React, { useState } from "react";
 
 /**
  * Использования этого компонента - пример композиции приложения,
@@ -13,23 +12,12 @@ import React, { useState } from "react";
  */
 function CostItem(props) {
 
-    const [description, setDescription] = useState(props.description); // деструктуризация - todo: попросить Артема обьяснить и есть ли аналог этой записи
-
-    function changeDescriptionHandler() {
-        // description = 'new text ..' & CostItem(props)
-        setDescription('new description text');
-        // console.log('costItemDescription : '+description);
-    }
-
   return (
     <Card className="cost-item">
       <CostDate date={props.date} />
       <div className="cost-item__description">
-        <h2>{description}</h2>
+        <h2>{props.description}</h2>
         <div className="cost-item__price">$ {props.amount}</div>
-          <button onClick={changeDescriptionHandler}>
-              CHange desription
-          </button>
       </div>
     </Card>
   );
