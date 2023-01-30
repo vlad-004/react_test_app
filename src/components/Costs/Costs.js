bfec6dd79f3b781845a8122936626990e1d14690
vlad-004 <prostoy-vlad@rambler.ru>
2023-01-30 20:42:14 +0200

    мини рефактор решения задания 3 после просмотра видеролика с решением от автора. Наверное эта версия более читаемая - мне пока это не понятно какой вариант лучше. Спросить у Артема.

--- src/components/Costs/Costs.js
+++ src/components/Costs/Costs.js
@@ -5,8 +5,6 @@ import CostFilter from '../CostFilter/CostsFilter'
 import React, {useState} from "react";
 
 const Costs = (props) => {
-    const costs = props.costs;
-
     const [filterYear, setFilterYear] = useState('');
     const filterYearChangeHandler = (year) => {
         setFilterYear(year);
@@ -28,25 +26,30 @@ const Costs = (props) => {
     //TODO: не могу понять почему при этой конструкции получаю в консоли ошибку но при этом все работает.
     //react-jsx-dev-runtime.development.js:87 Warning: Each child in a list should have a unique "key" prop.
 
+    /**
+     * Если в фильтре не указана дата(год) - тогда выводим все расходы за все время.
+     */
+    const filteredCosts = props.costs.filter(
+        (cost) => {
+            return filterYear
+                ? cost.date.getFullYear().toString() === filterYear
+                : true;
+        }
+    );
+
     return (
         <div>
             <Card className="costs">
                 <CostFilter filterYear={filterYear} onChangeYear={filterYearChangeHandler}/>
                 <h2>Композиция расходов</h2>
-                {costs
-                    .filter(
-                        (cost) => filterYear
-                            ? new Date(cost.date).getFullYear() == filterYear
-                            : cost
-                    )
-                    .map((cost) => (
-                        <CostItem
-                            key={cost.id}
-                            date={cost.date}
-                            description={cost.description}
-                            amount={cost.amount}
-                        />
-                    ))}
+                {filteredCosts.map((cost) => (
+                    <CostItem
+                        key={cost.id}
+                        date={cost.date}
+                        description={cost.description}
+                        amount={cost.amount}
+                    />
+                ))}
             </Card>
         </div>
     );
